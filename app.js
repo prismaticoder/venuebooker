eac93c78ea13ca6ddfa9bdd076eb4381e2b6387a
prismathic <pjjsalam1999@gmail.com>
2019-08-24 19:33:51 +0100

    Added db connection

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost:27017';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
